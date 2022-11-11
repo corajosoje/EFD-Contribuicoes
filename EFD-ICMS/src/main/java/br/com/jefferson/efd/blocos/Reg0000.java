f4d4ffbec46d66c73ac494d8e352dafe98da8cfb
88717 <88717@SPJUR-117796.netmaster.intra>
2022-11-11 11:07:37 -0300

    Add import for arg function

--- EFD-ICMS/src/main/java/br/com/jefferson/efd/blocos/Reg0000.java
+++ EFD-ICMS/src/main/java/br/com/jefferson/efd/blocos/Reg0000.java
@@ -47,8 +47,8 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Reg0000 implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Basic(optional = false)
     @Column(name = "ID")
     private Long id;
