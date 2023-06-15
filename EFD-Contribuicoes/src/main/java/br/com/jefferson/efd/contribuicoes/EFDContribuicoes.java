56e506fc4edbc1090c6854edc256639d565c0d8b
88717 <88717@SPJUR-117796.netmaster.intra>
2023-06-15 15:50:17 -0300

    Refactor and clean code

--- EFD-Contribuicoes/src/main/java/br/com/jefferson/efd/contribuicoes/EFDContribuicoes.java
+++ EFD-Contribuicoes/src/main/java/br/com/jefferson/efd/contribuicoes/EFDContribuicoes.java
@@ -4,7 +4,6 @@
  */
 package br.com.jefferson.efd.contribuicoes;//.EFDContribuicoes
 
-import br.com.jefferson.efd.contribuicoes.exceptions.LeitorException;
 import br.com.jefferson.efd.contribuicoes.exceptions.ScriptException;
 import br.com.jefferson.efd.contribuicoes.processamento.Leitor;
 import br.com.jefferson.efd.contribuicoes.processamento.ToSqlScript;
@@ -18,7 +17,7 @@ import java.nio.file.Paths;
 import org.apache.logging.log4j.LogManager;
 
 /**
- * @version 1.0
+ * @version 2.0
  * @author Jefferson Oliveira
  */
 public class EFDContribuicoes {
@@ -28,97 +27,235 @@ public class EFDContribuicoes {
     }
     private static final org.apache.logging.log4j.Logger log = LogManager.getLogger(EFDContribuicoes.class.getName());
     private static Leitor leitor;
+    private static String separator = File.separator;
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         try {
             if (args.length > 0) {
+
                 log.debug("Detectado " + args.length + " Argumento(s) na inicializacao");
-                if (args[0].equals("0")) {
-                    JPAConfiguration.createEntityManagerFactory().close();
-                    System.out.println("exit");
-                    System.exit(0);
-                }
 
-                File file = new File(args[0]);
-                if (!file.isDirectory()) {
-                    throw new LeitorException("O primeiro argumento deve ser uma pasta");
-                }
-
-                String separator = File.separator;
-                log.debug("Filtrando arquivos Sped");
-                File arquivos[] = file.listFiles(ObjectFactory.getSpedFileFilter());
-                log.info("Listado(s) " + arquivos.length + " arquivos");
-                if (arquivos.length > 0) {
-                    for (File arquivo : arquivos) {
-                        try {
-                            log.info("Processando arquivo: " + arquivo.getName());
-                            leitor = new Leitor(arquivo);
-                            if (args[1].equals("1")) {
-                                // Processamento para banco de dados
-                                if (!leitor.verificarDuplicidade()) {
-                                    log.info("Arquivo nao duplicado, quantidade de linhas: " + leitor.getLinhas());
-                                    processarArquivo(leitor);
-                                    log.info("Gravando Arquivo ...");
-                                    leitor.gravar();
-                                    log.info("Gravacao finalizada!");
-                                } else {
-                                    log.info("Arquivo Duplicado");
-                                }
-
-                                leitor.close();
-                                //fim do processamento
-                                log.info("Movendo arquivo: " + arquivo.getName());
-                                Path path = Paths.get(arquivo.getAbsolutePath());
-
-                                try {
-                                    File pasta = new File(path.getParent().toString() + separator + "importado");
-
-                                    if (!pasta.exists()) {
-                                        pasta.mkdir();
-                                    }
-                                    Files.move(path,
-                                            Paths.get(path.getParent().toString() + separator + "importado" + separator + path.getFileName()),
-                                            java.nio.file.StandardCopyOption.REPLACE_EXISTING);
-                                    log.info("Processamento do arquivo finalizado");
-
-                                } catch (Exception ex) {
-                                    log.error("Não foi possivel mover arquivo: " + arquivo.getName() + " " + ex.getMessage(), ex);
-                                }
-                            } else if (args[1].equals("2")) {
-                                log.info("Abrindo arquivo, quantidade de linhas: " + leitor.getLinhas());
-                                processarArquivo(leitor);
-                                //log.info("Iniciando gerador de Scripts");
-                                ToSqlScript gerar = new ToSqlScript(leitor.getSped());
-                                File destino = null;
-                                if (args.length == 2 || args[2] == null) {
-                                    destino = new File(arquivo.getPath().replace(".txt", ".sql"));
-                                } else {
-                                    destino = new File(args[2] + separator + arquivo.getName().replace(".txt", ".sql"));
-                                }
-                                gerar.gerarScript(destino);
-                                log.info("Script sql gerado com sucesso");
-
+                switch (args[0]) {
+                    case "0":
+                        JPAConfiguration.createEntityManagerFactory().close();
+                        System.out.println("exit");
+                        System.exit(0);
+                        break;
+                    case "1":
+                        if (args.length > 1) {
+                            if (args[1] == null || args[1].isBlank()) {
+                                System.out.println("Segundo parametro é obrigatório");
                             } else {
-                                throw new LeitorException("Funcao não implementada");
+                                gravarSPEDBanco(args[1]);
                             }
-                        } catch (IOException | ScriptException ex) {
-                            log.error("Não foi possivel processar arquivo: " + arquivo.getName(), ex);
+                        } else {
+                            System.out.println("Segundo parametro é obrigatório");
                         }
-                    }
-                } else {
-                    log.warn("Nenhum arquivo Sped encontrado");
+                        break;
+                    case "2":
+                        if (args.length > 1) {
+                            if (args[1] == null || args[1].isBlank()) {
+                                System.out.println("Segundo parametro é obrigatório");
+                            } else {
+                                if (args.length > 2) {
+                                    if (args[2] == null || args[2].isBlank() || args[1].equals(args[2])) {
+                                        gerarScritSQL(args[1]);
+                                    } else {
+                                        gerarScritSQL(args[1], args[2]);
+                                    }
+                                } else {
+                                    gerarScritSQL(args[1]);
+                                }
+
+                            }
+                        } else {
+                            System.out.println("Segundo parametro é obrigatório");
+                        }
+                        break;
+                    default:
+                        System.out.println("Nenhum Frame implementado");
+                        System.out.println("Função 0 - Gerar tabelas no banco de dados ");
+                        System.out.println("Função 1 - Processamento para banco de dados - 1;Pasta com arquivos SPED");
+                        System.out.println("Funcao 2 - Gerar script SQL - 2;Pasta com arquivos sped;[pasta destino]");
                 }
 
             } else {
                 System.out.println("Nenhum Frame implementado");
                 System.out.println("Função 0 - Gerar tabelas no banco de dados ");
-                System.out.println("Função 1 - Processamento para banco de dados (Pasta com arquivos sped) 1");
-                System.out.println("Funcao 2 - Gerar script SQL (Pasta com arquivos sped) 2 [pasta destino]");
+                System.out.println("Função 1 - Processamento para banco de dados - 1;Pasta com arquivos SPED");
+                System.out.println("Funcao 2 - Gerar script SQL - 2;Pasta com arquivos sped;[pasta destino]");
 
             }
         } catch (Exception ex) {
             log.error(ex.getMessage(), ex);
         }
+    }
+
+    private static void gerarScritSQL(String folder, String fileDestino) throws ScriptException {
+
+        File file = new File(folder);
+        File destino = new File(fileDestino);
+
+        if (!file.exists()) {
+            throw new ScriptException("Pasta origem não existe");
+        }
+        if (!file.isDirectory()) {
+            throw new ScriptException("Parametro origem não é uma pasta");
+        }
+        if (!file.canRead()) {
+            throw new ScriptException("Não é possivel Ler na pasta origem");
+        }
+        if (!destino.exists()) {
+            throw new ScriptException("Pasta destino não existe");
+        }
+        if (!destino.isDirectory()) {
+            throw new ScriptException("Parametro destino não é uma pasta");
+        }
+        if (!destino.canWrite()) {
+            throw new ScriptException("Não é possivel escrever na pasta destino");
+        }
+
+        log.debug("Filtrando arquivos Sped");
+        File arquivos[] = file.listFiles(ObjectFactory.getSpedFileFilter());
+        log.info("Listado(s) " + arquivos.length + " arquivos");
+        if (arquivos.length > 0) {
+            for (File arquivoSPED : arquivos) {
+                try {
+                    log.info("Processando arquivo: " + arquivoSPED.getName());
+                    leitor = new Leitor(arquivoSPED);
+
+                    log.info("Abrindo arquivo, quantidade de linhas: " + leitor.getLinhas());
+                    processarArquivo(leitor);
+                    //log.info("Iniciando gerador de Scripts");
+                    ToSqlScript gerar = new ToSqlScript(leitor.getSped());
+
+                    gerar.gerarScript(new File(fileDestino + separator + arquivoSPED.getName().replace(".txt", ".sql")));
+                    log.info("Script sql gerado com sucesso");
+
+                    try {
+                        log.info("Movendo arquivo: " + arquivoSPED.getName());
+                        moverArquivoProcessado(arquivoSPED);
+                    } catch (Exception ex) {
+                        log.warn("Não foi possivel mover arquivo: " + arquivoSPED.getName() + " " + ex.getMessage(), ex);
+                    }
+                } catch (IOException | ScriptException ex) {
+                    log.error("Não foi possivel processar arquivo: " + arquivoSPED.getName(), ex);
+                }
+            }
+        } else {
+            log.warn("Nenhum arquivo Sped encontrado");
+        }
+
+    }
+
+    private static void gerarScritSQL(String folder) throws ScriptException {
+
+        File file = new File(folder);
+
+        if (!file.exists()) {
+            throw new ScriptException("Pasta origem não existe");
+        }
+        if (!file.isDirectory()) {
+            throw new ScriptException("Parametro origem não é uma pasta");
+        }
+        if (!file.canRead()) {
+            throw new ScriptException("Não é possivel Ler na pasta origem");
+        }
+
+        log.debug("Filtrando arquivos Sped");
+        File arquivos[] = file.listFiles(ObjectFactory.getSpedFileFilter());
+        log.info("Listado(s) " + arquivos.length + " arquivos");
+        if (arquivos.length > 0) {
+            for (File arquivoSPED : arquivos) {
+                try {
+                    log.info("Processando arquivo: " + arquivoSPED.getName());
+                    leitor = new Leitor(arquivoSPED);
+
+                    log.info("Abrindo arquivo, quantidade de linhas: " + leitor.getLinhas());
+                    processarArquivo(leitor);
+                    //log.info("Iniciando gerador de Scripts");
+                    ToSqlScript gerar = new ToSqlScript(leitor.getSped());
+
+                    gerar.gerarScript(new File(folder + separator + "sql" + separator + arquivoSPED.getName().replace(".txt", ".sql")));
+                    log.info("Script sql gerado com sucesso");
+
+                    try {
+                        log.info("Movendo arquivo: " + arquivoSPED.getName());
+                        moverArquivoProcessado(arquivoSPED);
+                    } catch (Exception ex) {
+                        log.warn("Não foi possivel mover arquivo: " + arquivoSPED.getName() + " " + ex.getMessage(), ex);
+                    }
+                } catch (IOException | ScriptException ex) {
+                    log.error("Não foi possivel processar arquivo: " + arquivoSPED.getName(), ex);
+                }
+            }
+        } else {
+            log.warn("Nenhum arquivo Sped encontrado");
+        }
+
+    }
+
+    private static void gravarSPEDBanco(String folder) throws ScriptException {
+        File file = new File(folder);
+        if (!file.exists()) {
+            throw new ScriptException("Pasta origem não existe");
+        }
+        if (!file.isDirectory()) {
+            throw new ScriptException("Parametro origem não é uma pasta");
+        }
+        if (!file.canRead()) {
+            throw new ScriptException("Não é possivel Ler na pasta origem");
+        }
+        log.debug("Filtrando arquivos Sped");
+        File arquivos[] = file.listFiles(ObjectFactory.getSpedFileFilter());
+        log.info("Listado(s) " + arquivos.length + " arquivos");
+        if (arquivos.length > 0) {
+            for (File arquivo : arquivos) {
+                try {
+                    log.info("Processando arquivo: " + arquivo.getName());
+                    leitor = new Leitor(arquivo);
+
+                    // Processamento para banco de dados
+                    if (!leitor.verificarDuplicidade()) {
+                        log.info("Arquivo nao duplicado, quantidade de linhas: " + leitor.getLinhas());
+                        processarArquivo(leitor);
+                        log.info("Gravando Arquivo ...");
+                        leitor.gravar();
+                        log.info("Gravacao finalizada!");
+                    } else {
+                        log.info("Arquivo Duplicado");
+                    }
+
+                    leitor.close();
+                    //fim do processamento
+
+                    try {
+                        log.info("Movendo arquivo: " + arquivo.getName());
+                        moverArquivoProcessado(arquivo);
+                    } catch (Exception ex) {
+                        log.warn("Não foi possivel mover arquivo: " + arquivo.getName() + " " + ex.getMessage(), ex);
+                    }
+                    log.info("Processamento do arquivo finalizado");
+                } catch (IOException ex) {
+                    log.error("Não foi possivel processar arquivo: " + arquivo.getName(), ex);
+                }
+            }
+        } else {
+            log.warn("Nenhum arquivo Sped encontrado");
+        }
+
+    }
+
+    private static void moverArquivoProcessado(File arquivoSped) throws IOException {
+        Path path = Paths.get(arquivoSped.getAbsolutePath());
+        File pasta = new File(path.getParent().toString() + separator + "processado");
+
+        if (!pasta.exists()) {
+            pasta.mkdir();
+        }
+        Files.move(path,
+                Paths.get(path.getParent().toString() + separator + "processado" + separator + path.getFileName()),
+                java.nio.file.StandardCopyOption.REPLACE_EXISTING);
     }
 
     private static void processarArquivo(Leitor leitor) throws IOException {
