9a45aaa3fc1a34808e2efc96ad3f0f5bc7df9d77
88717 <88717@SPJUR-117796.netmaster.intra>
2023-06-29 17:58:15 -0300

    Add log in Exception

--- EFD-Contribuicoes/src/main/java/br/com/jefferson/efd/contribuicoes/exceptions/DaoException.java
+++ EFD-Contribuicoes/src/main/java/br/com/jefferson/efd/contribuicoes/exceptions/DaoException.java
@@ -1,21 +1,28 @@
 package br.com.jefferson.efd.contribuicoes.exceptions;
 
+import org.apache.logging.log4j.LogManager;
+
 /**
  * @version 1.0
  * @author Jefferson Oliveira
  */
 public class DaoException extends Exception {
 
+    private static final org.apache.logging.log4j.Logger log = LogManager.getLogger(DaoException.class.getName());
+
     public DaoException(String message) {
         super(message);
+        log.error(message);
     }
 
     public DaoException(Throwable ex) {
         super(ex);
+        log.error(ex);
     }
 
     public DaoException(String message, Throwable ex) {
         super(message, ex);
+        log.error(message, ex);
     }
 
 }
--- EFD-Contribuicoes/src/main/java/br/com/jefferson/efd/contribuicoes/exceptions/LeitorException.java
+++ EFD-Contribuicoes/src/main/java/br/com/jefferson/efd/contribuicoes/exceptions/LeitorException.java
@@ -1,20 +1,27 @@
 package br.com.jefferson.efd.contribuicoes.exceptions;
 
+import org.apache.logging.log4j.LogManager;
+
 /**
  * @version 1.0
  * @author Jefferson Oliveira
  */
 public class LeitorException extends RuntimeException {
 
+    private static final org.apache.logging.log4j.Logger log = LogManager.getLogger(LeitorException.class.getName());
+
     public LeitorException(String message) {
         super(message);
+        log.error(message);
     }
 
     public LeitorException(Throwable ex) {
         super(ex);
+        log.error(ex);
     }
 
     public LeitorException(String message, Throwable ex) {
         super(message, ex);
+        log.error(message, ex);
     }
 }
